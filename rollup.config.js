53ba7722608d19b0635c1398c159cfece5bfb180
Muhammad Redyka Fazry <fazry@theravenry.com>
2022-07-07 17:46:59 +0700

    Make react and react-dom as peer and dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -23,6 +24,7 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
