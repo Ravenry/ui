19b687195b3cc5fc19af96992b705e4af7e349cc
Rian Anugrah <rian.te.quiero@gmail.com>
2022-10-20 10:58:58 -0700

    NewCard Migrated and Storybook

--- src/Molecules/NewCard/NewCard.tsx
+++ src/Molecules/NewCard/NewCard.tsx
@@ -1,32 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { Segment } from '../../Atoms';
-import getElevation from '../../shared/elevations';
-import Responsive from '../../shared/responsive';
+import { Segment } from "../../Atoms";
+import getElevation from "../../shared/elevations";
+import Responsive from "../../shared/responsive";
+import { withProps } from "../../shared/withProps";
 
-const CardRoot = styled(Segment)`
-  margin-bottom: ${({ isMobile, noMargin }) => (isMobile ? '24px' : noMargin ? '0' : '24px')};
-  padding: ${({ padding }) => padding || '16px'};
+export interface CardRootProps extends NewCardProps {
+  isMobile?: boolean;
+  noMargin?: boolean;
+  column?: string;
+  borderTop?: string;
+  shadow?: string;
+  noHover?: boolean;
+  shadowOnHover?: boolean;
+}
+
+const CardRoot = withProps<CardRootProps>()(styled(Segment))`
+  margin-bottom: ${({ isMobile, noMargin }) =>
+    isMobile ? "24px" : noMargin ? "0" : "24px"};
+  padding: ${({ padding }) => padding || "16px"};
   cursor: pointer;
   display: flex;
-  width: ${({ isMobile, width }) => (isMobile ? width : '100%')};
-  flex-direction: ${({ column }) => (column ? 'column' : 'row')};
-  ${({ borderRadius }) => (borderRadius ? `border-radius: ${borderRadius};` : '')}
-  ${({ borderTop }) => (borderTop ? `border-top: ${borderTop};` : '')}
+  width: ${({ isMobile, width }) => (isMobile ? width : "100%")};
+  flex-direction: ${({ column }) => (column ? "column" : "row")};
+  ${({ borderRadius }) =>
+    borderRadius ? `border-radius: ${borderRadius};` : ""}
+  ${({ borderTop }) => (borderTop ? `border-top: ${borderTop};` : "")}
   ${({ shadow }) => (shadow ? `box-shadow: ${shadow};` : getElevation(2))}
-  ${({ height }) => (height ? `height: ${height};` : '')}
+  ${({ height }) => (height ? `height: ${height};` : "")}
 
-  ${({ noHover }) =>
+  ${({ noHover, shadowOnHover }) =>
     noHover
-      ? ''
+      ? ""
       : `&:hover {
-      ${({ shadowOnHover }) => (shadowOnHover ? `box-shadow: ${shadowOnHover};` : getElevation(4))}
+      ${shadowOnHover ? `box-shadow: ${shadowOnHover};` : getElevation(4)}
     }`}
 `;
 
-export default function Card(props) {
+export interface NewCardProps {
+  children?: any;
+}
+
+export default function Card(props: NewCardProps) {
   const { isMobile } = Responsive();
 
-  return <CardRoot isMobile={isMobile} {...props} />;
+  return (
+    <CardRoot isMobile={isMobile} {...props}>
+      {props.children}
+    </CardRoot>
+  );
 }
